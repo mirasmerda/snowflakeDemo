73aa120faf362eb335bcc8dfd022f41375f05ffd
Miroslav Smerda <miroslav_smerda@windowslive.com>
2020-05-12 21:22:22 +0200

    adapt t_link model to bq

--- models/load/t_links/t_link_transactions.sql
+++ models/load/t_links/t_link_transactions.sql
@@ -1,4 +1,4 @@
-{{- config(materialized='incremental', schema='VLT', tags='t_link') -}}
+{{- config(schema='vlt', tags='t_link') -}}
 
 {{ dbtvault.t_link(var('src_pk'), var('src_fk'), var('src_payload'), var('src_eff'),
                    var('src_ldts'), var('src_source'), var('source')) }}
