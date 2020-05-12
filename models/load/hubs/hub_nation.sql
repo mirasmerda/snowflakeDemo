48808070d576571478ae219bc2e1079b237267dd
Miroslav Smerda <miroslav_smerda@windowslive.com>
2020-05-12 21:08:58 +0200

    adapt hub models layer to bq

--- models/load/hubs/hub_nation.sql
+++ models/load/hubs/hub_nation.sql
@@ -1,4 +1,4 @@
-{{- config(materialized='incremental', schema='VLT', tags='hub') -}}
+{{- config(schema='vlt', tags='hub') -}}
 
 {{ dbtvault.hub(var('src_pk'), var('src_nk'), var('src_ldts'),
                 var('src_source'), var('source'))}}
