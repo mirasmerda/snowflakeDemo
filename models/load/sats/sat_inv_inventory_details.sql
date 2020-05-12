aafc865aff9438622472dc9b3eba0c9cabb9b87a
Miroslav Smerda <miroslav_smerda@windowslive.com>
2020-05-12 21:16:44 +0200

    adapt satellite models to bq

--- models/load/sats/sat_inv_inventory_details.sql
+++ models/load/sats/sat_inv_inventory_details.sql
@@ -1,4 +1,4 @@
-{{- config(materialized='incremental', schema='VLT', enabled=true, tags='satellite') -}}
+{{- config(schema='vlt', enabled=true, tags='satellite') -}}
 
 {{ dbtvault.sat(var('src_pk'), var('src_hashdiff'), var('src_payload'),
                 var('src_eff'), var('src_ldts'), var('src_source'),
